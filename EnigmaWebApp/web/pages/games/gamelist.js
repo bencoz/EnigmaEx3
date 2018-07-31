c238a6209d7c4a0cccbcc074006252ce42d33bef
edenhartman <34005340+edenhartman@users.noreply.github.com>
2018-07-31 15:53:26 +0300

    Add files via upload

--- EnigmaWebApp/web/pages/games/gamelist.js
+++ EnigmaWebApp/web/pages/games/gamelist.js
@@ -1,6 +1,7 @@
 
 
-var GAME_LIST_URL = buildUrlWithContextPath("gameslist");
+var GAME_LIST_URL = buildUrlWithContextPath("../games/gameslist");
+var JOIN_GAME_URL = buildUrlWithContextPath("gamepage");
 var refreshRate = 2000; //mili seconds
 
 function refreshGamesList(games) {
@@ -14,24 +15,56 @@ function refreshGamesList(games) {
         //game will be described by jason:
         //{battlefieldName : name, managerName: managerName, isActive:is/isn't(true/false), difficulty:difficulty,
         //actualNumOfUsers:actualNumOfUsers , neededNumOfUsers:neededNumOfUsers}
-        $('<div>',{class: "gameObj",id: game.battlefieldName}
-            .append($('<ul>')
-                .append('<li>' + "Battlefield Name: "+ game.battlefieldName+'</li>')
-                .append('<li>' + "Manager Name: "+ game.managerName+'</li>')
-                .append('<li>' + "Game" + game.isActive + "active" +'</li>')
-                .append('<li>' + "Difficulty Level" + game.difficulty  +'</li>')
-                .append('<li>' + game.actualNumOfUser + "/" + NeededNumOfUser +'</li>'))
-            .appendTo($("#gameslist")));
+        $('<button>',{ type : "button", onclick : "clickOnGame()"})
+            .append('<div>',{class: "gameObj",id: game.battlefieldName}
+                .append($('<ul>')
+                    .append('<li>' + "Battlefield Name: "+ game.battlefieldName+'</li>')
+                    .append('<li>' + "Manager Name: "+ game.managerName+'</li>')
+                    .append('<li>' + "Game" + game.isActive + "active" +'</li>')
+                    .append('<li>' + "Difficulty Level" + game.difficulty  +'</li>')
+                    .append('<li>' + game.actualNumOfUser + "/" + NeededNumOfUser +'</li>'))
+                .appendTo($("#gameslist")));
     });
 }
 
 function ajaxGamesList() {
     $.ajax({
-        url: GAME_LIST_URL,
+        url: "./gamelist",
         success: function(games) {
             refreshGamesList(games);
         }
     });
+
+
+}
+
+function clickOnGame()
+{
+    var t = this;
+    $.ajax({
+    method:'POST',
+    url: "/pages/joingame",
+
+    data: {battleName:name},
+    timeout: 4000,
+    error: function(xhr) {
+        var html = $.parseHTML(xhr.responseText)
+        console.error("Failed to submit");
+        if (html)
+            $("#result").text(html[5].innerText);
+    },
+    success: function(data) {
+        console.log("joined game");
+        $("#result").empty();
+        var newDoc = document.open("text/html", "replace");
+        newDoc.write(data);
+        //$("html").html(data);
+    }
+});
+    // return value of the submit operation
+    // by default - we'll always return false so it doesn't redirect the user.
+    return false;
+
 }
 
 $(function() {
