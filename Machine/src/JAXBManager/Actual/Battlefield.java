0af47ec94b345617f57085878bc921c18c16d9de
edenhartman <34005340+edenhartman@users.noreply.github.com>
2018-08-01 17:33:10 +0300

    Add files via upload

--- Machine/src/JAXBManager/Actual/Battlefield.java
+++ Machine/src/JAXBManager/Actual/Battlefield.java
@@ -2,8 +2,6 @@ package JAXBManager.Actual;
 
 import Factory.DifficultyLevel;
 
-import java.io.InputStream;
-
 public class Battlefield {
     private DifficultyLevel level;
     private String name;
@@ -11,7 +9,7 @@ public class Battlefield {
     private int numOfRounds;
 
     public Battlefield(String i_name, int i_numOfAllies, String i_level, Integer i_numOfRounds){
-        level = DifficultyLevel.getDufficultyByStr(i_level);
+        level = DifficultyLevel.getDifficultyByStr(i_level);
         name = i_name;
         numOfAllies = i_numOfAllies;
         if (i_numOfRounds != null) {
@@ -22,7 +20,7 @@ public class Battlefield {
     }
 
     public Battlefield(JAXBManager.JAXBGenerated.Battlefield i_battlefield){
-        level = DifficultyLevel.getDufficultyByStr(i_battlefield.getLevel());
+        level = DifficultyLevel.getDifficultyByStr(i_battlefield.getLevel());
         name = i_battlefield.getBattleName();
         numOfAllies = i_battlefield.getAllies();
         numOfRounds = i_battlefield.getRounds();
